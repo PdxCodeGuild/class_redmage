76dde70bee4a2185c2307038fe75ed6716cdbb01
Chad Bean <crb4u@yahoo.com>
2018-10-16 07:49:22 -0700

    upload new turtle creation

--- code/chad/turtle_own_creation.py
+++ code/chad/turtle_own_creation.py
@@ -1,6 +1,6 @@
 from turtle import *
 
-speed(1000)
+speed(500)
 
 i = 0
 while i < 73:
@@ -12,33 +12,64 @@ x = 0
 penup()
 setposition(30,80)
 pendown()
+fillcolor('black')
+begin_fill()
 while x < 40:
     forward(2)
     right(10)
     x += 1
+end_fill()
 
 penup()
 setposition(-10,80)
 pendown()
+fillcolor('black')
+begin_fill()
 y = 0
 while y < 40:
     forward(2)
     right(10)
     y += 1
+end_fill()
 
 penup()
-setposition(-10,30)
+setposition(-25,40)
 pendown()
 
 forward(20)
-left(20)
+left(40)
 forward(20)
-left(20)
+left(40)
 forward(20)
-left(20)
+left(40)
 forward(20)
-left(20)
+left(40)
 forward(20)
-left(20)
+left(40)
+
+penup()
+setposition(0,0)
+pendown()
+
+
+right(215)
+forward(100)
+right(45)
+forward(75)
+right(180)
+forward(75)
+right(90)
+forward(75)
+right(180)
+forward(75)
+right(45)
+forward(50)
+right(45)
+forward(50)
+left(180)
+forward(50)
+right(90)
+forward(50)
+
 
 done()
