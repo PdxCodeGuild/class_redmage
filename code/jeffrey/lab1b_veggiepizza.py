97582cde211699701b26c46566865c8316f84714
jeffrey <jeff.r.leslie@gmail.com>
2018-10-16 12:42:16 -0700

    reloading lab1b

--- code/jeffrey/lab1b_veggiepizza.py
+++ code/jeffrey/lab1b_veggiepizza.py
@@ -1,136 +1,137 @@
-from turtle import *
+import turtle as tur
 
 # Veggie pizza in the sky over the great pyramids of Giza.
 
-speed(0)
+tur.speed(0)
 
-fillcolor('orange')
-begin_fill()
+tur.fillcolor('orange')
+tur.begin_fill()
 
 i = 0
 x = 25
 while i < 33:
-    left(45)
-    forward(x)
-    right(90)
-    forward(x)
-    left(45)
+    tur.left(45)
+    tur.forward(x)
+    tur.right(90)
+    tur.forward(x)
+    tur.left(45)
     i = i + 11
     x = x*2
-    penup()
-    setposition(i*2,0)
-    pendown()
+    tur.penup()
+    tur.setposition(i*2,0)
+    tur.pendown()
 
-setposition(0,0)
-end_fill()
+tur.setposition(0,0)
+tur.end_fill()
 
-penup()
-setposition(-25,40)
-pendown()
-fillcolor('brown')
-begin_fill()
-circle(30)
-end_fill()
+tur.penup()
+tur.setposition(-25,40)
+tur.pendown()
+tur.fillcolor('brown')
+tur.begin_fill()
+tur.circle(30)
+tur.end_fill()
 
-penup()
-setposition(-25,45)
-pendown()
+tur.penup()
+tur.setposition(-25,45)
+tur.pendown()
 
-fillcolor('yellow')
-begin_fill()
-circle(25)
-end_fill()
-left(90)
-forward(50)
-right(180)
-forward(25)
-right(90)
-forward(25)
-right(180)
-forward(50)
-right(180)
-forward(25)
-right(45)
-forward(25)
-right(180)
-forward(50)
-right(180)
-forward(25)
-right(90)
-forward(25)
-right(180)
-forward(50)
+tur.fillcolor('yellow')
+tur.begin_fill()
+tur.circle(25)
+tur.end_fill()
+tur.left(90)
+tur.forward(50)
+tur.right(180)
+tur.forward(25)
+tur.right(90)
+tur.forward(25)
+tur.right(180)
+tur.forward(50)
+tur.right(180)
+tur.forward(25)
+tur.right(45)
+tur.forward(25)
+tur.right(180)
+tur.forward(50)
+tur.right(180)
+tur.forward(25)
+tur.right(90)
+tur.forward(25)
+tur.right(180)
+tur.forward(50)
 
-penup()
-setposition(-30,55)
-pendown()
-
-edge_length = 0
-i = 0
-while i < 10:
-    forward(edge_length)
-    right(154)
-    i = i+1
-    edge_length += 1
-
-penup()
-setposition(-31,75)
-pendown()
+tur.penup()
+tur.setposition(-30,55)
+tur.pendown()
 
 edge_length = 0
 i = 0
 while i < 10:
-    forward(edge_length)
-    right(154)
+    tur.forward(edge_length)
+    tur.right(154)
     i = i+1
     edge_length += 1
 
-penup()
-setposition(-23,80)
-pendown()
+tur.penup()
+tur.setposition(-31,75)
+tur.pendown()
 
 edge_length = 0
 i = 0
 while i < 10:
-    forward(edge_length)
-    right(154)
+    tur.forward(edge_length)
+    tur.right(154)
     i = i+1
     edge_length += 1
 
-penup()
-setposition(-15,57)
-pendown()
+tur.penup()
+tur.setposition(-23,80)
+tur.pendown()
 
 edge_length = 0
 i = 0
 while i < 10:
-    forward(edge_length)
-    right(154)
+    tur.forward(edge_length)
+    tur.right(154)
     i = i+1
     edge_length += 1
 
-penup()
-setposition(-13,71)
-pendown()
+tur.penup()
+tur.setposition(-15,57)
+tur.pendown()
 
 edge_length = 0
 i = 0
 while i < 10:
-    forward(edge_length)
-    right(154)
+    tur.forward(edge_length)
+    tur.right(154)
     i = i+1
     edge_length += 1
 
-penup()
-setposition(-36,67)
-pendown()
+tur.penup()
+tur.setposition(-13,71)
+tur.pendown()
 
 edge_length = 0
 i = 0
 while i < 10:
-    forward(edge_length)
-    right(154)
+    tur.forward(edge_length)
+    tur.right(154)
     i = i+1
     edge_length += 1
 
+tur.penup()
+tur.setposition(-36,67)
+tur.pendown()
+
+edge_length = 0
+i = 0
+while i < 10:
+    tur.forward(edge_length)
+    tur.right(154)
+    i = i+1
+    edge_length += 1
+
+tur.done()
 done()
