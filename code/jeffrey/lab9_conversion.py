4a0a40e9ef0f98276caebad1bbe8430483180cda
jeffrey <jeff.r.leslie@gmail.com>
2018-10-17 16:33:22 -0700

    updates

--- code/jeffrey/lab9_conversion.py
+++ code/jeffrey/lab9_conversion.py
@@ -1,7 +1,10 @@
-user_input = float(input("[F-M] What is the distance in feet? "))
+user_input = float(input("What is the distance in feet (integers only)? "))
 
-conv_value = user_input * 0.3048
+feet_meter = user_input * 0.3048
 
-conv_value = round(conv_value,4)
+feet_meter = round(feet_meter,4)
 
-print(str(user_input) + " in feet is " + str(conv_value) + " in meters!")
+print(str(user_input) + " in feet is " + str(feet_meter) + " in meters!")
+
+user_input2 = input("What is the distance? ")
+user_input3 = input("what are the units? ")
