37b91dcb08eff4ed619db4f5dfab911d97e783e0
scottdharvey <scottharvey@gmail.com>
2018-10-17 11:46:15 -0700

    change maker takes float dollar input and returns qtrs, dime, nickels, pennies

---
+++ code/scott/lab8_makechange.py
@@ -0,0 +1,28 @@
+#lab8_makechange.py
+
+# priming
+user_input = 0
+
+# getting number of pennies
+while user_input <= 0:
+    user_input = float(input("How much money do you want to convert to change? (format: #.##)>"))
+
+# convert from dollar form float to integer
+user_input *= 100
+user_input = int(user_input)
+
+# defining a function to make change by removing values in decreasing order
+def change(user_input):
+    qtrs = user_input // 25
+    balance = user_input % 25
+    dimes = balance // 10
+    balance = balance % 10
+    nickels = balance // 5
+    balance = balance % 5
+    return qtrs, dimes, nickels, balance
+
+# running the function with the user's input
+changed = change(user_input)
+
+#printing the results.
+print(f"You get back {changed[0]} quarters, {changed[1]} dimes, {changed[2]} nickels, and {changed[3]} pennies")
