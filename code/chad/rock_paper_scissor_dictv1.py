0f2d6cfb6988502faa21c4f9e3d6a954e9cdd23b
Chad Bean <crb4u@yahoo.com>
2018-10-17 07:34:56 -0700

    added rock papper scissors

--- code/chad/rock_paper_scissor_dictv1.py
+++ code/chad/rock_paper_scissor_dictv1.py
@@ -2,14 +2,14 @@ import random, time
 
 lookup = {'rock': 'paper', 'paper': 'scissors', 'scissors': 'rock'}
 
-user_input = 'scissors'
+user_input = input('Choose Rock, Paper, Scissors')
 user_input = user_input.lower()
 print(f'You Chose {user_input}')
 print('\n' * 5)
 time.sleep(1)
 
 computer_values = ['rock', 'paper', 'scissors']
-computer_choice = 'paper'
+computer_choice = random.choice(computer_values)
 print(f'Computer Chooses {computer_choice}')
 time.sleep(1)
 print('\n' * 5)
