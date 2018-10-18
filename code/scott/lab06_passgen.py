dadbd04ff5f3cf9eb36b1069e3c97f10e986b5e1
scottdharvey <scottharvey@gmail.com>
2018-10-18 15:15:49 -0700

    passgen with all options

--- code/scott/lab06_passgen.py
+++ code/scott/lab06_passgen.py
@@ -4,20 +4,50 @@
 import random
 import string
 
-# generating a ranom string from which to get characters for the password (punctuation added twice to increase likelihood of special characters)
-pass_string = string.punctuation + string.ascii_letters + string.punctuation
-
 # priming
-input_num = 0
-start_num = 0
-new_pass = ""
+usercaps = ""
+userlower = ""
+usersym = ""
+capstring = string.ascii_uppercase
+lowerstring = string.ascii_lowercase
+symstring = string.punctuation
+password = ""
 
-# making sure the user is inputting a valid number
-while input_num not in range (10, 21):
-    input_num = int(input("How long do you want your password to be? Please choose a number between 10 and 20 :"))
+# collecting desired values
+while usercaps == "":
+    usercaps = input("How many capital letters do you want in your password? (enter a number or 'none'): ")
+    if usercaps == "none":
+        break
+    else:
+        usercaps = int(usercaps)
 
-# iterate up to the numbere entered by the user
-for i in range(0, input_num):
-    new_pass += str(random.choice(pass_string))
+while userlower == "":
+    userlower = input("How many lowercase letters do you want in your password? (enter a number or 'none'): ")
+    if userlower == "none":
+        break
+    else:
+        userlower = int(userlower)
 
-print(new_pass)
+while usersym == "":
+    usersym = input("How many special characters do you want in your password? (enter a number or 'none'): ")
+    if usersym == "none":
+        break
+    else:
+        usersym = int(usersym)
+
+
+# randomly selecting from the right list
+if usercaps != "none":
+        for i in range(abs(usercaps)):
+           password = password + random.choice(capstring)
+if userlower != "none":
+    for i in range(abs(userlower)):
+            password = password + random.choice(lowerstring)
+if usersym != "none":
+    for i in range(abs(usersym)):
+        password = password + random.choice(symstring)
+
+password = list(password)
+random.shuffle(password)
+pass_string = "".join(password)
+print(f"Your new password is {pass_string}")
