077d695a6fe5c4d0c3fa750f0f818965f6698493
jeffrey <jeff.r.leslie@gmail.com>
2018-10-18 12:09:16 -0700

    adding labs

--- code/jeffrey/lab9_conversion.py
+++ code/jeffrey/lab9_conversion.py
@@ -1,3 +1,5 @@
+import string
+
 user_input = float(input("What is the distance in feet (integers only)? "))
 
 feet_meter = user_input * 0.3048
@@ -6,5 +8,48 @@ feet_meter = round(feet_meter,4)
 
 print(str(user_input) + " in feet is " + str(feet_meter) + " in meters!")
 
-user_input2 = input("What is the distance? ")
-user_input3 = input("what are the units? ")
+print("\nEnd Version 1\n")
+
+to_meters_conversion = {
+    'ft' : .3048,
+    'mi' : 1609.34,
+    'm' : 1,
+    'km' : 1000,
+    'yd' : .9144,
+    'in' : .0254,
+}
+
+user_dist = float(input("What is the numeric distance? "))
+user_unit = str(input("What are the units [ft, mi, m, km, yd, or in]? "))
+
+while user_unit not in to_meters_conversion.keys():
+    print("Invalid unit entry; Please type unit type according to unit in following list: what are the units [ft, mi, m, km, yd, or in]")
+    user_unit = str(input("What are the units? "))
+
+conversion = user_dist  * to_meters_conversion[user_unit]
+
+print("\nThat's "+ str(conversion) + " m.\n")
+
+print("End version 2 & 3\n")
+
+user_dist = float(input("What is the numeric distance? "))
+user_unit = str(input("What are the units [ft, mi, m, km, yd, or in]? "))
+
+while user_unit not in to_meters_conversion.keys():
+    print("Invalid unit entry; Please type unit type according to unit in following list: what are the units [ft, mi, m, km, yd, or in]")
+    user_unit = str(input("What are the units? "))
+
+user_unit_conv = str(input("what unit would you like to convert to [ft, mi, m, km, yd, or in]? "))
+while user_unit_conv not in to_meters_conversion.keys():
+    print("Invalid unit entry; Please type unit type according to unit in following list: what are the units [ft, mi, m, km, yd, or in]")
+    user_unit_conv = str(input("What are the units? "))
+
+
+conversion = user_dist  * to_meters_conversion[user_unit]
+conv2 = conversion / to_meters_conversion[user_unit_conv]
+
+print("\nYour converted value is {0:.2f}".format(conv2) + " " + user_unit_conv)
+
+# print(str(user_dist) + " " + user_unit + " is "+ str(conv2) + user_unit_conv+"\n")
+
+print("\nEnd version 4!")
---
+++ code/jeffrey/notes.py
@@ -0,0 +1,4 @@
+numbers = [34, 53, 22, 16, 77, 94, 56]
+
+for i in range(len(numbers)):
+        print(numbers[i], end =" ")
