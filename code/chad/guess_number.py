44e3e8bb0775eef8393c4baa5e7e68cd1a176864
root <root@debian.beanpamilya.com>
2018-10-18 14:47:36 -0700

    added some more shit

--- code/chad/guess_number.py
+++ code/chad/guess_number.py
@@ -2,13 +2,11 @@ import random
 number_guess = random.randint(1,10)
 x = 0
 while x < 11:
-    print (number_guess)
     user_guess = int(input('Guess a number between 1 and 10, you will have 10 tries: > '))
     if number_guess == user_guess:
         print('You Won The Game!!')
         break
     else:
         x += 1
-        print('you got here')
         continue
   
---
+++ code/chad/guess_numberv3.py
@@ -0,0 +1,19 @@
+import random
+number_guess = random.randint(1,10)
+times_tried = 0
+
+istrue = True
+while istrue:
+    user_guess = int(input('Guess a number between 1 and 10, you will have 10 tries: > '))
+    if number_guess == user_guess:
+        print('You Won The Game!!')
+        istrue = False
+    else:
+        times_tried += 1
+        print(f'You Have Tried {times_tried}')
+        if user_guess > number_guess:
+            print('You Are Too High')
+        if user_guess < number_guess:
+            print('You Are Too Low')
+        continue
+  
