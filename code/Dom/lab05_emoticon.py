64946254c590b71b6a0f7a2e0ff329a47379f99d
DAMIAN ORAMAS <damianoramas@DAMIANs-MBP.localdomain>
2018-10-18 11:22:15 -0700

    update to emoticon gen with dictionaries

--- code/Dom/lab05_emoticon.py
+++ code/Dom/lab05_emoticon.py
@@ -3,8 +3,7 @@ import random
 eye_list = [":", ";", "=","B"]
 nose_list = ["^", "-","", "*"]
 mouth_list =[")", "P", "(", "/" ]
-#emoticon = random.choice(eye_list) + random.choice(nose_list) + random.choice(mouth_list)
-#print(emoticon)
+
 for i in range(5):
     emoticon = random.choice(eye_list) + random.choice(nose_list) + random.choice(mouth_list)
     print(emoticon)
