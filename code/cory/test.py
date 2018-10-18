c4a4606d99c620fe85184fcabd84a8b653db5f0e
Cory Morrissey <corydm92@gmail.com>
2018-10-18 12:47:56 -0700

    update files

---
+++ code/cory/test.py
@@ -0,0 +1,4 @@
+end = input("Would you like to continue? > (y/n)")
+while end != 'y' and end != 'n':
+    end = input("YOU FUCKING NEED TO PRESS ONE OF THESE TWO LETTERS? > (y/n)")
+print("You ended the loop!")
