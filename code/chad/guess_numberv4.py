e7ede4ae54793d12fd879a05331e526953b282e3
root <root@debian.beanpamilya.com>
2018-10-18 15:39:50 -0700

    adding more and more shit

---
+++ code/chad/guess_numberv4.py
@@ -0,0 +1,25 @@
+import random
+number_guess = random.randint(1,10)
+times_tried = 0
+previous_guess = 0
+
+istrue = True
+while istrue:
+    user_guess = int(input('Guess a number between 1 and 10, you will have 10 tries: > '))
+    current_closer = abs(user_guess-number_guess)
+    previous_closer = abs(previous_guess-number_guess)
+    if number_guess == user_guess:
+        print('You Won The Game!!')
+        istrue = False
+    else:
+        print('you got here')
+        previous_guess = user_guess     
+        times_tried += 1
+        print(f'You Have Tried {times_tried}')
+        if current_closer >= previous_closer:
+            print('You Were Closer The Previous Time You Played')
+        else:
+            print('You were Closer This Time THan Last time')
+        
+        continue
+  
