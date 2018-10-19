f4165481e79cbc9b0d0afb98a4a1d546a598ca73
jeffrey <jeff.r.leslie@gmail.com>
2018-10-19 10:08:08 -0700

    pasta!

--- code/jeffrey/lab7_rpsgame.py
+++ code/jeffrey/lab7_rpsgame.py
@@ -1,2 +1,11 @@
 import random as ran
 
+g_choices = ["rock","paper","scissors"]
+
+p_start = input("Do you want to play a game? ")
+
+while p_start != "no":
+    user_choice = input("Do you plan to throw rock, paper, or scissors? ")
+    computer_choice = ran.choice(g_choices)
+    if user_choice == computer_choice:
+        print("tie!") 
