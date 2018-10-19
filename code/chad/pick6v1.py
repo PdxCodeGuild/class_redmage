6e4f13f4879c0e0c3a38559ca8c0fcda74eb044e
root <root@debian.beanpamilya.com>
2018-10-19 16:33:21 -0700

    adding some shit

--- code/chad/pick6v1.py
+++ code/chad/pick6v1.py
@@ -1,11 +1,53 @@
 import random
 
 computer_picked = []
+user_picked = []
 money_balance = 0
-track_num = 1
+wins = 0
 
-while track_num < 7:
-    computer_pick = random.randint(0,100)
-    computer_picked.append(computer_pick)
-    track_num += 1
-    print(computer_picked)
+def computer_choices(computer_picked):
+    track_num = 1
+    while track_num < 7:
+        computer_pick = random.randint(0,100)
+        computer_picked.append(computer_pick)
+        track_num += 1
+        if track_num == 7:
+            return computer_picked
+
+def user_choices(user_picked):
+    track_num = 1
+    while track_num < 7:
+        user_pick = random.randint(0,100)
+        user_picked.append(user_pick)
+        track_num += 1
+        if track_num == 7:
+            return user_picked
+
+def ifwon(user_picked, computer_picked, wins):
+    if computer_picked[0] == user_picked[0]:
+        wins += 2
+    if computer_picked[1] == user_picked[1]:
+        wins += 1   
+    if computer_picked[2] == user_picked[2]:
+        wins += 1 
+    if computer_picked[3] == user_picked[3]:
+        wins += 1   
+    if computer_picked[4] == user_picked[4]:
+        wins += 1   
+    if computer_picked[5] == user_picked[5]:
+        wins += 1
+    return wins
+
+
+x = 0
+
+while x < 10:
+    print(user_choices(user_picked))
+    print(computer_choices(computer_picked))
+    print(ifwon(user_picked, computer_picked, wins))
+    x += 1
+
+print(f'You Won {wins} Times!!!')
+
+
+    
--- code/chad/test_code.py
+++ code/chad/test_code.py
@@ -1,25 +1,2 @@
-import random
-number_guess = random.randint(1,10)
-times_tried = 0
-previous_guess = 0
-
-
-while True:
-    user_guess = int(input('Guess a number between 1 and 10, you will have 10 tries: > '))
-    current_closer = abs(user_guess-number_guess)
-    previous_closer = abs(previous_guess-number_guess)
-    if number_guess == user_guess:
-        print('You Won The Game!!')
-        break
-    else:
-        print('you got here')
-        previous_guess = user_guess     
-        times_tried += 1
-        print(f'You Have Tried {times_tried} Times')
-        if current_closer >= previous_closer:
-            print('You Were Closer The Previous Time You Played')
-        else:
-            print('You were Closer This Time Than Last time')
-        
-        continue
-  
+def test (a,b='hi', c, d='up'):
+    print(a,b,c,d)
