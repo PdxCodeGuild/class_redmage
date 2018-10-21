2a98bdd7b8a12976ad9787d1cd8e7ea6ff84181f
Cory Morrissey <corydm92@gmail.com>
2018-10-21 09:53:37 -0700

    update files

---
+++ code/cory/practice 2.py
@@ -0,0 +1,31 @@
+# Problem 1
+'''
+def hello(letter1, letter2, letter3, letter4, letter5):
+    return letter1*2 + letter2*2 + letter3*2 + letter4*2 + letter5*2
+print(hello('h', 'e', 'l', 'l', 'o'))
+'''
+# Problem 2
+
+# Problem 3
+'''
+def latest_letter(letter1):
+    let_list = list(letter1)
+    let_list.sort()
+    return let_list[-1]
+
+print(latest_letter('pneumonoultramicroscopicsilicovolcanocovniosis'))
+'''
+
+# Problem 4 < NOT DONE
+'''
+def count_hi(letter):
+    x = letter.split()
+    print(x)
+    return letter.count(letter)
+
+print(count_hi('hihihi'))
+'''
+
+# Problem 5
+
+
