3896fa277760a07aee4d3b21b90ba966f14fb83a
Chad Bean <crb4u@yahoo.com>
2018-10-21 15:00:28 -0700

    adding pick6 update

--- code/chad/pick6v1.py
+++ code/chad/pick6v1.py
@@ -1,8 +1,10 @@
-import random
+import random, time
+f= open("Lottery_Results.txt","w+")
 
-
-#money_balance = 0
+money_balance = 0
 wins = 0
+returned_win_won = ()
+counter = 0
 
 def get_choices():
     picked = []
@@ -11,42 +13,70 @@ def get_choices():
         picked.append(pick)
     return picked
 
+def money_calc(wins):
+    money_balance = 0
+    print(wins)
+    if wins == 0:
+        money_balance -= 1
+    elif wins == 1:
+        money_balance += 4
+    elif wins == 2:
+        money_balance += 7
+    elif wins == 3:
+        money_balance += 100
+        print('You Won Big 100')
+        #input('Paused You Won $100 Jackpot')
+    elif wins == 4:
+        money_balance += 50000
+        print('You Won Big $50k')
+        input('Paused You Won $50k Jackpot')
+        f.write('You Won The 50k Jackpot')
+        f.close()
+    elif wins == 5:
+        money_balance += 1000000
+        print('You Won Big $100k')
+        input('Paused You Won $100k Jackpot')
+    elif wins == 6:
+        money_balance += 25000000
+        print('You Won Big $25 MILLON')
+        input('Paused You Won The Jackpot')
+    print(money_balance)
+    return money_balance
 
-#after each wins it starts over and doesnt go up in value
+#Why if i put a 0 in for index does it still work fine?
 def ifwon(user_picked, computer_picked):
     wins = 0
-    if computer_picked[0] == user_picked[0]:
-        wins += 1
-    if computer_picked[1] == user_picked[1]:
-        wins += 1
-    if computer_picked[2] == user_picked[2]:
-        wins += 1
-    if computer_picked[3] == user_picked[3]:
-        wins += 1
-    if computer_picked[4] == user_picked[4]:
-        wins += 1
-    if computer_picked[5] == user_picked[5]:
-        wins += 1
-    return wins
+    money_won = 0
+    return_win_won = ()
+    for index in range(7):
+        if computer_picked[index] == user_picked[index]:
+            wins += 1
+        money_won = (money_calc(wins))
+        return_win_won = wins, money_won
+    return return_win_won
 
 
 
-# why does my lists get appended when if i dont clear them at the end of this loop?
-for num in range(1000):
+
+for num in range(1000000):
     computer_picked = get_choices()
     user_picked = get_choices()
-    print(computer_picked)
-    print(user_picked)
-    #user_picked = []
-    #print(user_choices(user_picked))
-    #im calling the function ifwon and expecting it to return wins, pass it back and save as "won"
-    wins += ifwon(user_picked, computer_picked)
-#    user_picked = []
-#    computer_picked = []
+    returned_win_won = ifwon(user_picked, computer_picked)
+    returned_win_won = list(returned_win_won)
+    counter += 1
+    wins += returned_win_won[0]
+    money_balance += returned_win_won[1]
+
+
+
+
+
 
 
 print('You won ' + str(wins) + ' Times')
-
+print(f'You Now Have {money_balance} Dollars Total Money')
+print(f'You Have Played {counter} Many Times')
+print(f'Your Average is Win Rate is {money_balance / counter} ')
 
 
 
