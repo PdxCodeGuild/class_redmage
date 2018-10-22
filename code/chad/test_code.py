d6771966fc35b80ca39ac63caeb81295f416dd94
Chad Bean <crb4u@yahoo.com>
2018-10-21 21:04:05 -0700

    updating pick6

--- code/chad/test_code.py
+++ code/chad/test_code.py
@@ -1,17 +1,4 @@
-
-listNames = ['Dan', 'Neil', 'Chad', 'David', 'Zuriel', 'Tom']
-listNames2 = ['Dan', 'Chad', 'Chad', 'David', 'Zuriel', 'Tom']
-
-
-enumList1 = list(enumerate(listNames))
-enumList2 = list(enumerate(listNames2))
-
-for test_value in range(6):
-
-   if enumList1[test_value] == enumList2[test_value]:
-      print('won')
-   else:
-      print('You Loose')
+total_won = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0}
 
 
 
