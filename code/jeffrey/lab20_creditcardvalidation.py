a6f274736152e50779b11774ae4c325a00f18551
jeffrey <jeff.r.leslie@gmail.com>
2018-10-24 12:07:26 -0700

    wadu hek

--- code/jeffrey/lab20_creditcardvalidation.py
+++ code/jeffrey/lab20_creditcardvalidation.py
@@ -1,24 +1,36 @@
 while True:
     try:
         cred_input = input("Enter your credit card number: ")
-        cred_input = list(cred_input)
-        cc_num = list(map(int, cred_input))
+        # print(f"The cc# you entered is: {cred_input}")
+        cred_in = list(cred_input)
+        cc_num = list(map(int, cred_in))
     except ValueError:
         print("Please enter a valid credit card #!")
         continue
     else:
         break
 
-while True:
-    last_dig = cc_num[-1]
-
-    cc_num = cc_num[:len(cc_num)-1]
-
-    cc_num = cc_num[::-1]
-
-    for i in cc_num(range(0,len(cc_num-1),2)):
-        i*=2
-    #need to do the double every other here.
-    # either use a range and then only double on the "i"s or index's from the range that I created, can use range parameters
-    # or use enumerate and then the % to see if the number is divisible by 2
-    
+def cc_valid(credit_number):
+    flag = True
+    while flag:
+        last_dig = credit_number[-1]
+        credit_number = credit_number[:len(credit_number)-1]
+        credit_number = credit_number[::-1]
+        for i in range(0,len(credit_number),2):
+            credit_number[i] *= 2
+        for i in credit_number:
+            if i > 9:
+                i -= 9
+        sum_val = sum(credit_number)
+        sum_val_list = str(sum_val)
+        sum_val_list_dig2 = sum_val_list[1]
+        flag = False
+    if sum_val_list_dig2 == last_dig:
+        return True
+    else:
+        return False
+        
+if cc_valid(cc_num) == True:
+    print("Valid!")
+else:
+    print("Invalid!")
