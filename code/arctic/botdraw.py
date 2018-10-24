3c8430cd17c24e082e5e9bd86bfb922826342772
ArcticAzn <43914781+ArcticAzn@users.noreply.github.com>
2018-10-24 10:21:40 -0700

    Add files via upload

---
+++ code/arctic/botdraw.py
@@ -0,0 +1,50 @@
+# test
+from turtle import *
+i = 0
+while i < 4:
+    forward(100)
+    left(90)
+    i = i + 1
+speed(5)
+penup()
+forward(50)
+right(90)
+pendown()
+forward(50)
+right(90)
+forward(75)
+x = 0
+while x < 3:
+    left(90)
+    forward(150)
+    x = x +1
+left(90)
+forward(75)
+penup()
+forward(75)
+left(45)
+pendown()
+forward(150)
+penup()
+left(135)
+forward(361)
+pendown()
+left(135)
+forward(150)
+penup()
+left(135)
+forward(150)
+pendown()
+left(45)
+forward(250)
+penup()
+right(135)
+forward(325)
+right(90)
+forward(176)
+pendown()
+left(135)
+forward(250)
+
+done()
+
