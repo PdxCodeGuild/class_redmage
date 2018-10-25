a2b7a77a5daad15d6d95d2e0a547bf865302db23
Cory Morrissey <corydm92@gmail.com>
2018-10-25 15:10:51 -0700

    jack2 final.. final. final

--- code/jack2.py
+++ code/jack2.py
@@ -3,7 +3,7 @@ import random
 sex_list = ['male', 'female']
 
 #jack_list_template = [{'name':'jack', 'age':0, 'sex:'m', 'preg':False}]
-jack_list = [{'name': 'adam', 'age': 0, 'sex': 'male', 'preg': 0}, {'name': 'eve', 'age': 0, 'sex': 'female', 'preg': 0}]
+jack_list = [{'name': 'adam', 'age': 0, 'sex': 'male', 'preg': 0}, {'name': 'eve', 'age': 0, 'sex': 'female', 'preg': 0}, {'name': 'adam2', 'age': 0, 'sex': 'male', 'preg': 0}, {'name': 'eve2', 'age': 0, 'sex': 'female', 'preg': 0}]
 
 def create_new_jack():
     jack_list = {'name': 'jack', 'age': 0, 'sex': random.choice(sex_list), 'preg': 0}
@@ -15,36 +15,48 @@ def preg_funck(i):
             if jack_list[i] != jack_list[0]:
                 if jack_list[i-1]['sex'] == 'male' and jack_list[i-1]['age'] in range(4,9):
                     return True
-                # Had lint say 'break not used properly in loop', changed to 'pass'
-                else:
-                    pass
-                    # break
+
             if jack_list[i] != jack_list[len(jack_list) - 1]:
                 if jack_list[i+1]['sex'] == 'male' and jack_list[i+1]['age'] in range(4,9):
                     return True
-    
+
+            return False
+
+        return False
+
+    return False
+
 year = 0   
 
-while year <= 6:
-    # for i in range(len(jack_list)):
-    #     jack_list[i]['preg'] = False
-
+while len(jack_list) > 0:
+   
     for i in range(len(jack_list)):
-        
+
         jack_list[i]['age'] += 1
-
+        
         if preg_funck(i):
-            
+        
             jack_list[i]['preg'] += 1
-
+        
             if jack_list[i]['preg'] == 2:
-                
+        
                 jack_list.append(create_new_jack())
                 jack_list[i]['preg'] = 0
 
-    # random.shuffle(jack_list)
+    new_list = sorted(jack_list, key=lambda k: k['age']) 
 
+    for i in range((len(new_list) - 1), -1 , -1):
+
+        if new_list[i]['age'] == 10:
+            new_list.remove(new_list[i])
+
+    jack_list = new_list
+    random.shuffle(jack_list)
     year += 1
 
-    print(jack_list)
+    # print(len(jack_list))
+    # print(year)
+    # print('\n')
+    print(f"Year {year}, {len(jack_list)} is population.")
+
 
