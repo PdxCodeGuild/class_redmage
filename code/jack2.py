265555a10ad6de977cdd48839a0bc28a7a1aa58d
Cory Morrissey <corydm92@gmail.com>
2018-10-25 12:44:43 -0700

    jack2 update

--- code/jack2.py
+++ code/jack2.py
@@ -3,37 +3,48 @@ import random
 sex_list = ['male', 'female']
 
 #jack_list_template = [{'name':'jack', 'age':0, 'sex:'m', 'preg':False}]
-jack_list = []
-#
+jack_list = [{'name': 'adam', 'age': 0, 'sex': 'male', 'preg': 0}, {'name': 'eve', 'age': 0, 'sex': 'female', 'preg': 0}]
+
 def create_new_jack():
-    jack_list = [{'name':'jack'+str(random.randint(0,100)), 'age':0, 'sex':random.choice(sex_list), 'preg':False}]
+    jack_list = {'name': 'jack', 'age': 0, 'sex': random.choice(sex_list), 'preg': 0}
     return jack_list
+
 def preg_funck(i):
     if jack_list[i]['sex'] == 'female':
-        if jack_list[i]['age'] in range(4,9) and jack_list[i]['preg'] == False:
+        if jack_list[i]['age'] in range(4,9) and jack_list[i]['preg'] == 0 or 1:
             if jack_list[i] != jack_list[0]:
                 if jack_list[i-1]['sex'] == 'male' and jack_list[i-1]['age'] in range(4,9):
                     return True
+                # Had lint say 'break not used properly in loop', changed to 'pass'
                 else:
-                    break
-            if jack_list[i] != len(jack_list-1):
+                    pass
+                    # break
+            if jack_list[i] != jack_list[len(jack_list) - 1]:
                 if jack_list[i+1]['sex'] == 'male' and jack_list[i+1]['age'] in range(4,9):
                     return True
     
-def do_loop():
-    for i in len(jack_list):
-        #Calls preg_func function above
-        if preg_funck(i) is True:
+year = 0   
 
+while year <= 6:
+    # for i in range(len(jack_list)):
+    #     jack_list[i]['preg'] = False
+
+    for i in range(len(jack_list)):
+        
+        jack_list[i]['age'] += 1
+
+        if preg_funck(i):
             
+            jack_list[i]['preg'] += 1
 
+            if jack_list[i]['preg'] == 2:
+                
+                jack_list.append(create_new_jack())
+                jack_list[i]['preg'] = 0
 
+    # random.shuffle(jack_list)
 
+    year += 1
 
+    print(jack_list)
 
-
-#Step 1 call function to create a new jack 
-jack_list.append(create_new_jack())
-print(jack_list)
-#Step 2 is Create The Loop
-do_loop()
