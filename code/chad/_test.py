3f61702669a98d9fdec8f8d45f6c5bac5fcd86ac
root <root@debian.beanpamilya.com>
2018-10-26 14:59:49 -0700

    adding

---
+++ code/chad/_test.py
@@ -0,0 +1,16 @@
+import string
+
+nums = string.digits
+
+print(nums)
+
+print(type(nums))
+
+while True:
+    user_input = input('give me somehting')
+    if user_input in nums
+        continue
+    if len(user_input) != 1:
+        continue
+    else:
+        break
