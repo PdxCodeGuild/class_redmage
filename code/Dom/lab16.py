f12a97b9f9db55efa55744849579cf494b520295
DAMIAN ORAMAS <damianoramas@DAMIANs-MBP.localdomain>
2018-10-29 09:56:47 -0700

    rounding

--- code/Dom/lab16.py
+++ code/Dom/lab16.py
@@ -9,10 +9,10 @@ pixels = img.load()
 for i in range(width):
     for j in range(height):
         r, g, b = pixels[i, j]
-        y = 0.299*r + 0.587*g + 0.114*b
-        r =int(y)
-        g =int(y)
-        b =int(y)
+        y =round(0.299*r + 0.587*g + 0.114*b)
+        r =y
+        g =y
+        b =y
 
 
         pixels[i, j] = (r, g, b)
