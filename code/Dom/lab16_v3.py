345354832c85b58c77bbf71e137bc2555712ee5f
DAMIAN ORAMAS <damianoramas@DAMIANs-MBP.localdomain>
2018-10-29 11:41:17 -0700

    stick

--- code/Dom/lab16_v3.py
+++ code/Dom/lab16_v3.py
@@ -16,24 +16,24 @@ draw.rectangle(((0, 0), (width, height)), fill="white")
 #draw.rectangle(((100, 100), (300, 300)), fill="black")
 
 #body arms and legs
-draw.line((250, 200, 250, 350), fill="black")
-draw.line((250, 350, 125, height), fill="black")
-draw.line((250, 350, 375, height), fill="black")
-draw.line((100, 250, 400, 250), fill="black")
-
+draw.line((250, 200, 250, 350), fill="black", width= 10)
+draw.line((250, 350, 200, height), fill="black",width=10)
+draw.line((250, 350, 300, height), fill="black",width=10)
+draw.line((270, 255, 400, 300), fill="black",width=10)
+draw.line((230, 255, 100, 300), fill="black",width=10)
 draw.rectangle(((225, 250), (275, 350)), fill="black")
 #the head
 circle_x = width/2
 circle_y = height/4
-circle_radius = 100
+circle_radius = 75
 draw.ellipse((circle_x-circle_radius, circle_y-circle_radius, circle_x+circle_radius, circle_y+circle_radius), fill='yellow', outline='black')
 #the eyes
-circle_x = 200
+circle_x = 225
 circle_y = 100
 circle_radius = 10
 draw.ellipse((circle_x-circle_radius, circle_y-circle_radius, circle_x+circle_radius, circle_y+circle_radius), fill='black', outline='black')
 
-circle_x = 300
+circle_x = 275
 circle_y = 100
 circle_radius = 10
 draw.ellipse((circle_x-circle_radius, circle_y-circle_radius, circle_x+circle_radius, circle_y+circle_radius), fill='black', outline='black')
