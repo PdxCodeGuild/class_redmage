e9c44d0e2b1fbbd43c4489a19ca23c11ab99d61f
DAMIAN ORAMAS <damianoramas@DAMIANs-MBP.localdomain>
2018-10-29 09:42:55 -0700

    gray scale

--- code/Dom/lab16.py
+++ code/Dom/lab16.py
@@ -2,7 +2,7 @@
 from PIL import Image
 # import PIL
 
-img = Image.open("class_redmage/code/dom/Lenna_(test_image).png")
+img = Image.open("/Users/damianoramas/Desktop/redmage/class_redmage/code/dom/Lenna_(test_image).png")
 width, height = img.size
 pixels = img.load()
 
@@ -10,6 +10,10 @@ for i in range(width):
     for j in range(height):
         r, g, b = pixels[i, j]
         y = 0.299*r + 0.587*g + 0.114*b
+        r =int(y)
+        g =int(y)
+        b =int(y)
+
 
         pixels[i, j] = (r, g, b)
 img.show()
