e0b3674daf3c1bba43cfff94570a0230f5fba786
jeffrey <jeff.r.leslie@gmail.com>
2018-10-29 09:39:56 -0700

    yep

--- code/jeffrey/lab16_image_change.py
+++ code/jeffrey/lab16_image_change.py
@@ -7,11 +7,28 @@ for i in range(width):
     for j in range(height):
         r, g, b = pixels[i, j]
 
-        y = int(0.299*r + 0.587*g + 0.114*b)        
-        r = y
-        b = y
-        g = y
+#         y = int(0.299*r + 0.587*g + 0.114*b)        
+#         r = y
+#         b = y
+#         g = y
 
-        pixels[i, j] = (r, g, b)
+#         pixels[i, j] = (r, g, b)
+
+# img.show()
+
+import colorsys
+
+# colorsys uses colors in the range [0, 1]
+h, s, v = colorsys.rgb_to_hsv(r/255, g/255, b/255)
+
+# do some math on h, s, v
+
+r, g, b = colorsys.hsv_to_rgb(h, s, v)
+
+# convert back to [0, 255]
+
+r = int(r*255)
+g = int(g*255)
+b = int(b*255)
 
 img.show()
