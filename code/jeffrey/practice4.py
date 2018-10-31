8a6a067d5fb58370e9d529d6fd9a0f966d2ee14d
jeffrey <jeff.r.leslie@gmail.com>
2018-10-31 15:47:00 -0700

    wadoop

--- code/jeffrey/practice4.py
+++ code/jeffrey/practice4.py
@@ -20,12 +20,19 @@ print(round(average(combine(fruits,prices)),2))
 
 # Problem #3
 d = {'a1':5, 'a2':2, 'a3':3, 'b1':10, 'b2':1, 'b3':1, 'c1':4, 'c2':5, 'c3':6}
-# unify(d) -> {'a':3, 'b':4, 'c':5}
 
-def unify(x):
-    new_dict = {sum(x.values())/len(x.values)}
+def unify(my_dict):
+
+    new_dict = {}
+# for the key (variable) in my dict
+    for key in my_dict:
+        if key[0] in new_dict:
+            new_dict[key[0]].append(my_dict[key])
+        else:
+            new_dict[key[0]] = [my_dict[key]]
     
-    # for keys,values in x if (x.keys())[i][0] == (x.keys())[i][0]}
+    new_dict = {k:round((sum(v)/len(v)),2) for k,v in new_dict.items()}
+    
     return new_dict
 
 print(unify(d))
--- code/jeffrey/test.py
+++ code/jeffrey/test.py
@@ -1,5 +1,17 @@
 d = {'a1':5, 'a2':2, 'a3':3, 'b1':10, 'b2':1, 'b3':1, 'c1':4, 'c2':5, 'c3':6}
-a = list(d.items())
 
+def unify(my_dict):
 
-print(a)
+    new_dict = {}
+# for the key (variable) in my dict
+    for key in my_dict:
+        if key[0] in new_dict:
+            new_dict[key[0]].append(my_dict[key])
+        else:
+            new_dict[key[0]] = [my_dict[key]]
+    
+    new_dict = {k:round((sum(v)/len(v)),2) for k,v in new_dict.items()}
+    
+    return new_dict
+
+print(unify(d))
