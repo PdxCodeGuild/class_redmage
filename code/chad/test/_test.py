06c0935513663860f0bff98aca8010bb55ccb805
Chad Bean <crb4u@yahoo.com>
2018-10-31 15:08:57 -0700

    adding lab23

--- code/chad/test/_test.py
+++ code/chad/test/_test.py
@@ -1,2 +1,4 @@
-for line in contact_list:
-    new_list.append(dict(contact_list[0], ))
+index = {'name': 'Chad Bean', ' address': ' 1516 NE Hancock', ' address 2': ' Apt 402', ' city': ' Portland', ' state': ' OR', ' zip': '97202', ' phone': ' 310-867-5182', ' email': ' crb4u@yahoo.com'}
+
+for i in index:
+    print(index[i])
