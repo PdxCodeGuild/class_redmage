0e2d34a77446a30709aa294cf16ec6dda766c747
Chad Bean <crb4u@yahoo.com>
2018-11-10 13:08:11 -0800

    “final”

--- code/chad/python_final_project/blank_rename_file.py
+++ code/chad/python_final_project/blank_rename_file.py
@@ -1,14 +1,15 @@
 import os, random
 
-source = '/home/chad/Documents/git_hub/class_redmage/code/chad/python_final_project/doc_send/'
-destination = '/home/chad/Documents/git_hub/class_redmage/code/chad/python_final_project/doc_send/'
-
+source = '/Users/chadbean/Documents/Programing and Development/github/class_redmage/code/chad/python_final_project/doc_send/'
+destination = '/Users/chadbean/Documents/Programing and Development/github/class_redmage/code/chad/python_final_project/doc_send/'
+counter = 0
 for filename in os.listdir(source):
     print(filename)
     src = source + filename
-    dst = destination + 'testblank' + str(random.randint(0,1000)) + '.pdf'
+    dst = destination + 'testblank' + str(counter) + '.pdf'
     os.rename(src,dst)
-
+    counter += 1
+print(counter)
     
 # for filename in os.listdir(src):
 #     print(filename)
