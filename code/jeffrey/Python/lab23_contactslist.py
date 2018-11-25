23b2cdb72d1df1d5800301ac2e9cf4b5a3a526d7
jeffrey <jeff.r.leslie@gmail.com>
2018-11-24 17:01:16 -0800

    updates

--- code/jeffrey/Python/lab23_contactslist.py
+++ code/jeffrey/Python/lab23_contactslist.py
@@ -1,3 +1,5 @@
+import time
+
 # opens a CSV file with write capability and reads it to variable: lines while splitting it on each line
 with open('contact_list.csv', 'r+') as file:
     lines = file.read().split('\n')
@@ -25,30 +27,52 @@ def create():
     user_val2 = input(f'Input the {(keys[1])}: ')
     user_val3 = input(f'Input the {(keys[2])}: ')
     
-    entry_str = (user_val1,user_val2,user_val3)
+    entry_str = (user_val1.lower(),user_val2.lower(),user_val3.lower())
     
-    checker = input(f'Enter "yes" if you want to add: {entry_str}')
+    checker = input(f"Enter 'no if you don't want to add: {entry_str}")
     
-    if checker.lower() == 'yes':
+    if checker.lower() != 'no':
         entry = dict(zip(keys,entry_str))
         contacts_dict.append(entry)
-
+        time.sleep(.5)
+        return contacts_dict
+    print('Record created!\n')
+    
 def retrieve():
-    user_val1 = input(f'What is the {(keys[0])} of the record that you would like to retrieve? ').lower()
+    user_val1 = input(f'What is the {keys[0]} of the record that you would like to retrieve? ').lower()
+
     for i in range(len(contacts_dict)):
-        if user_val1 == contacts_dict[i][(keys[0])]:
-            print(f'\n{contacts_dict[i]}\n')
-            return contacts_dict
+        if user_val1 == contacts_dict[i][keys[0]]:
+            time.sleep(.5)
+            print(f'\nRecord: {contacts_dict[i]}\n')
+            time.sleep(.5)
+            return
 
 # def update():
 def update():
-    pass
+    record = input(f'What is the {keys[0]} of the record that you would like to update?').lower()
+    attribute = input(f'What is the attribute that you would like to set: "{keys[0]}", "{keys[1]}", or "{keys[2]}"?').lower()
+    new_attribute = input(f'What would you like to change the "{attribute}" to? ')
+    if attribute in keys:
+        for i in range(len(contacts_dict)):
+            if record == contacts_dict[i][keys[0]]:
+                contacts_dict[i][attribute] = new_attribute
+                time.sleep(.5)
+                print("Update complete!\n")
+                return contacts_dict
 
 # def delete():
 def delete():
-    pass
+    record = input(f'What is the {keys[0]} of the record that you would like to delete?')
+    for i in range(len(contacts_dict)):
+        if record == contacts_dict[i][keys[0]]:
+            contacts_dict.remove(contacts_dict[i])
+            time.sleep(.5)
+            print("Record deleted.")
+            return contacts_dict
 
 while True:
+    print(contacts_dict)
     choice = input(f'''
     Welcome {user_name}, you have four options for actions:
     1. 'create' for making a new record
@@ -61,10 +85,10 @@ while True:
         create()
     elif choice.lower() == 'retrieve':
         retrieve()
-    # elif choice.lower() == 'update':
-    #     update()
-    # elif  choice.lower() == 'delete':
-    #     delete()
+    elif choice.lower() == 'update':
+        update()
+    elif  choice.lower() == 'delete':
+        delete()
     end_prog = input('Input any key to do another action or "quit" to exit the program:\n')
     if end_prog.lower() == 'quit':
         break
