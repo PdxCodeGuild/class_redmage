91ffc1edca0d1fd0ef7f1bd572110f1a30e1d04f
DAMIAN ORAMAS <damianoramas@MacBook-Pro.localdomain>
2018-11-30 13:39:52 -0800

    stuff

--- code/Dom/Javascript labs/lab02  guess the number js/version 2/app.js
+++ code/Dom/Javascript labs/lab02  guess the number js/version 2/app.js
@@ -19,23 +19,23 @@ function checkguess() {
     console.log(guessNum);
     ++guessNum;
     let message = document.getElementById("message");{
-    if (userGuess === num) {  
+        if (userGuess === num) {  
             // game = false; 
             message.innerText= `You won and it only took you ${guessNum} tries!`;
-    }else if (userGuess !== num) {
-        if (guessNum === 1) {
-            if (userGuess < num) {
+        }else if (userGuess !== num) {
+            if (guessNum === 1) {
+                if (userGuess < num) {
                     lastGuess = userGuess;
                     message.innerText= "too low, try again";
-            }else if (userGuess > num) {
+                }else if (userGuess > num) {
                     lastGuess = userGuess;
                     message.innerText="too high, try again";
-            }
-        } else {
-            if (Math.abs(num - userGuess) < Math.abs(num -lastGuess)) {
+                }
+            } else {
+                    if (Math.abs(num - userGuess) < Math.abs(num -lastGuess)) {
                     lastGuess = userGuess;
                     message.innerText="Getting warmer, try again.";
-            }else if (Math.abs(num - guess) > Math.abs(num -lastGuess)) {
+                    }else if (Math.abs(num - guess) > Math.abs(num -lastGuess)) {
                     lastGuess = guess;
                     message.innerText="Getting colder, try again.";
                 } 
