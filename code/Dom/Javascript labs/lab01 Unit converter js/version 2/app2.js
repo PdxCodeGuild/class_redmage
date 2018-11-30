9238c8fc1fcb1827c4603512184853658accb212
DAMIAN ORAMAS <damianoramas@MacBook-Pro.localdomain>
2018-11-30 10:56:53 -0800

    lab1 webapp

--- code/Dom/Javascript labs/lab01 Unit converter js/version 2/app2.js
+++ code/Dom/Javascript labs/lab01 Unit converter js/version 2/app2.js
@@ -1,14 +1,18 @@
 // #unit_converter.py lab
-let toMeters = { ft: 0.3048, mi: 1609.34, m :1, km :1000, yd :0.9144, in: 0.0254};
-
-let num = document.getElementById("num");
 
 
-let inUnit = document.getElementById("inUnit");
+function conversion(e) {
+    let num = document.getElementById("num");
+    let n = num.value;
+    let inUnit = document.getElementById("inUnit");
+    let i = inUnit.value
+    let outUnit = document.getElementById("outUnit");
+    let o = outUnit.value;
+    let toMeters = { ft: 0.3048, mi: 1609.34, m :1, km :1000, yd :0.9144, in: 0.0254};
+    let convertedAnswer = document.getElementById("answer");
+    let conversion = Number((n * toMeters[i]/toMeters[o]).toFixed(5));
+    convertedAnswer.innerText= `${n} ${i} equals ${conversion} ${o}`;
+}
 
-let outUnit = document.getElementById("outUnit");
-
-function conversion(num, inUnit, outUnit) {
-    return num * toMeters[inUnit]/toMeters[outUnit]}
-
-alert(`${num} ${inUnit} equals ${conversion(num, inUnit, outUnit).toFixed(2)} ${outUnit}`)
+let btn = document.querySelector('#btn');
+btn.addEventListener('click', conversion);
