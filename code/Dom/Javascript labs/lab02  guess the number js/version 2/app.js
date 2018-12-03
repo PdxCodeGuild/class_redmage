432040e17c94840462acac133959804b08dcda1d
DAMIAN ORAMAS <damianoramas@MacBook-Pro.localdomain>
2018-12-03 09:55:24 -0800

    lab 2 guess num

--- code/Dom/Javascript labs/lab02  guess the number js/version 2/app.js
+++ code/Dom/Javascript labs/lab02  guess the number js/version 2/app.js
@@ -1,46 +1,68 @@
 // #guess_the_number
-let num = Math.floor(Math.random()*21);
-let guessNum = 0
-let lastGuess = 0
+let num = Math.floor((Math.random()*21)+1);
+let guessNum = 0;
+let lastGuess = 0;
 // let game = true;
 let btn = document.querySelector('#btn');
-alert(num);
-// keeps game going?
-// 
+console.log(num)
+
+function reset() {
+    num = Math.floor(Math.random()*21);
+    guessNum = 0;
+    lastGuess = 0; 
+    console.log(num);
+    let message= document.getElementById("message");
+    message.innerText ="";
+    let btn2 = document.getElementById("btn2");
+    let container_div = document.getElementById('container');
+    container_div.removeChild(btn2);
+
+}
 
 btn.addEventListener('click', checkguess);
+
 // 
 // function to check the guess d
 // 
 function checkguess() {
     let guess = document.getElementById("guess");
     let userGuess = parseInt(guess.value);
-    console.log(`num is ${num} user guess is ${userGuess}, last guess is ${lastGuess}`);
-    console.log(guessNum);
     ++guessNum;
     let message = document.getElementById("message");{
         if (userGuess === num) {  
-            // game = false; 
             message.innerText= `You won and it only took you ${guessNum} tries!`;
+            guess.style.backgroundColor="";
+            let btn2 = document.createElement("button");
+            btn2.type = "button";
+            btn2.setAttribute("class","btn btn-info");
+            btn2.setAttribute("id","btn2");
+            btn2.innerText = 'Play Again';
+            let container_div = document.getElementById('container');
+            container_div.appendChild(btn2);
+            btn2.addEventListener('click', reset);
         }else if (userGuess !== num) {
             if (guessNum === 1) {
-                if (userGuess < num) {
+                if ((Math.abs(num - userGuess)) < 5)  {
                     lastGuess = userGuess;
-                    message.innerText= "too low, try again";
-                }else if (userGuess > num) {
+                    message.innerText= "warm";
+                    guess.style.backgroundColor= "darkgoldenrod";
+                }else if ((Math.abs(num - userGuess)) >= 5) {
                     lastGuess = userGuess;
-                    message.innerText="too high, try again";
+                    message.innerText="cold";
+                    guess.style.backgroundColor= "lightskyblue"
                 }
             } else {
-                    if (Math.abs(num - userGuess) < Math.abs(num -lastGuess)) {
+                    if ((Math.abs(num - userGuess)) < (Math.abs(num -lastGuess))) {
                     lastGuess = userGuess;
                     message.innerText="Getting warmer, try again.";
-                    }else if (Math.abs(num - guess) > Math.abs(num -lastGuess)) {
-                    lastGuess = guess;
+                    guess.style.backgroundColor= "orange";
+                    }else if ((Math.abs(num - userGuess)) > (Math.abs(num -lastGuess))) {
+                    lastGuess = userGuess;
                     message.innerText="Getting colder, try again.";
+                    guess.style.backgroundColor="skyblue"
                 } 
             }
         }
-    }
+    }; guess.value = "";
 }
 
