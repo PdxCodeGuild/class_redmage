98f39ece34e0869215e6bc19c406d885378b5775
jeffrey <jeff.r.leslie@gmail.com>
2018-12-03 16:29:26 -0800

    u wot m8

--- code/jeffrey/JavaScript/lab25_atm.js
+++ code/jeffrey/JavaScript/lab25_atm.js
@@ -1,88 +1,87 @@
 let cash;
 let flag;
-let user_input;
 
 class Atm {
 
     // initializes with balance and sets interest rate default
-  constructor (self, balance = 0, int_rate = .001) {
-      this.balance= balance;
-      this.int_rate= int_rate;
-      this.trans = [];
+  constructor (balance = 0, int_rate = .001, trans = []) {
+      this.balance = balance;
+      this.int_rate = int_rate;
+      this.trans = trans;
   }
 
   // returns the account balance
-  check_balance() {
+  checkBalance() {
       return this.balance;
   }
 
   // deposits the given amount in the account
-  deposit() {
+  deposit(amount) {
       this.balance += amount;
-      this.trans.push(`${amount} deposited`);
+      this.trans.push(`$${amount} deposited`);
       return this.balance;
   }
 
   // returns true if the withdrawn amount won't put the account in the negative
-  __check_withdrawal(){
+  checkWithdrawal(amount){
       if ((this.balance - amount) >= 0) {
-          return True;
+          return true;
       } else {
-          return False;
+          return false;
         }
   }
 
   // withdraws the amount from the account and returns it
-  withdraw() {
-      if (this.__check_withdrawal(amount)) {
+  withdraw(amount) {
+      if (this.checkWithdrawal(amount) === true) {
         this.balance -= amount;
-        this.trans.push(`${amount} withdrawn`);
-        return self.balance;
+        this.trans.push(`$${amount} withdrawn`);
+        return this.balance;
       } else {
-        return "Error: Not enough money to withdraw.";
+        return ("Error: Not enough money to withdraw that amount.");
       }
   }
 
   // returns the amount of interest calculated on the account
-  calc_interest() {
+  calcinterest() {
       return (this.balance * this.int_rate);
   }
 
   // prints out a history of transactions
-  print_transactions() {
-      return `Transactions: ${self.trans.join(", ")}`;
-      }
+  printTransactions() {
+    if (this.trans.length == 0) {
+      return ("No transactions yet.");
+    } else {
+      return (`Transactions: ${this.trans.join(", ")}`);
+    }
+  }
 }
 
-
-cash = ATM(500);
-flag = True;
+cash = new Atm(500);
+flag = true;
 alert("Thank you for using VERYSECUREDEFINITELYNOTFRAUD ATM\nWhat action would you like to do:\n1 for Deposit\n2 for Withdraw\n3 for Check Balance\n4 for Transaction History\n5 for Finish");
 
 while (flag) {
-  user_input = int(prompt('>'));
+  user_input = parseInt(prompt('>'));
 
-  if (user_input == 1) {
-    try {
-      deposit_amount = int(prompt("How much would you like to deposit: $"));
-      cash.deposit(deposit_amount);
-    }
-    catch {
-      alert('Enter a valid amount.');
-    }
-  }else if (user_input == 2) {   withdraw_amount = int(prompt("How much would you like to withdraw: $"))
-    try{
-      if ("Error" in cash.withdraw(withdraw_amount)){
+  if (user_input === 1) {
+    amount = parseFloat(prompt("How much would you like to deposit: $"));
+    alert(`Account credited with $${amount}.`)
+    cash.deposit(amount);
+  } else if (user_input === 2) {
+      amount = parseFloat(prompt("How much would you like to withdraw: $"))
+      if (cash.checkWithdrawal(amount) === false){
         alert("Not enough funds for that withdrawal amount.");
-    }
-  } catch {
-    alert(`Account debited by ${withdraw_amount}.`)
-    }
-    } else if (user_input == 3) {
-      alert(`***Your balance on this account is ${cash.check_balance()}***`)
-    } else if (user_input == 4) {
-      alert(cash.print_transactions())
-    } else if (user_input == 5) {
-      flag = False;
-    }
+      } else {
+        cash.withdraw(amount);
+        alert(`Account debited by $${amount}.`)}
+  } else if (user_input === 3) {
+      alert(`***Your balance on this account is ${cash.checkBalance()}***`)
+  } else if (user_input === 4) {
+      alert(cash.printTransactions())
+  } else if (user_input === 5) {
+      flag = false;
   }
+}
+
+// *****************deal with NaN*************************
