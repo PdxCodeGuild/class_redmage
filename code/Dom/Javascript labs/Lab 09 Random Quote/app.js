44e0eec822ff2b3c1ed80238392ae67580910533
DAMIAN ORAMAS <damianoramas@MacBook-Pro.localdomain>
2018-12-10 15:06:26 -0800

    added pages

--- code/Dom/Javascript labs/Lab 09 Random Quote/app.js
+++ code/Dom/Javascript labs/Lab 09 Random Quote/app.js
@@ -1,5 +1,11 @@
 let target = document.getElementById("target");
 let Btn = document.getElementById("button");
+let sTarget = document.getElementById("s-results");
+let searchBtn = document.getElementById("search-btn");
+let search = document.getElementById("search");
+let previous = document.getElementById("previous");
+let next = document.getElementById("next");
+let pageNum = document.getElementById("page-num");
 
 Btn.addEventListener("click", function(e){
     let req = new XMLHttpRequest();
@@ -24,3 +30,56 @@ req.addEventListener("load", function(e) {
 req.open("GET", "https://favqs.com/api/qotd");
 req.send();
 })
+
+
+
+
+searchBtn.addEventListener("click", function(e){
+    let req = new XMLHttpRequest();
+    let searchText = search.value;
+    let pageId = 1;
+    let quotes = document.getElementsByTagName("li");
+    for (let i=(quotes.length -1); i >= 0; i--){
+        sTarget.removeChild(quotes[i]);      // removes old list when new search is initiated
+    }
+req.addEventListener("progress", function(e) {
+    console.log(`Request ${e.loaded/e.total * 100}% complete`)
+});
+
+req.addEventListener("error", function (e){
+    sTarget.innerText =`oh no error ${e.status}`;
+});
+
+req.addEventListener("load", function(e) {
+    let response = JSON.parse(req.responseText);
+    pageNum.innerText = `pg. ${pageId}`;
+    for (let i=0; i< response.quotes.length; i++){
+        let li = document.createElement("li");
+        li.innerHTML = `${response.quotes[i].body}<i><a href="${response.quotes[i].url}"> ~${response.quotes[i].author}</a></i></p>`;
+        sTarget.appendChild(li);
+    }
+});
+next.addEventListener("click", function(){
+    pageId += 1;
+    for (let i=(quotes.length -1); i >= 0; i--){
+        sTarget.removeChild(quotes[i]);};   
+    req.open("GET", `https://favqs.com/api/quotes?page=${pageId}&filter=${searchText}`);
+    req.setRequestHeader("Authorization", `Token token="${apiKey}"`);
+    req.send();
+});
+previous.addEventListener("click", function(){
+    if (pageId >1 ){
+    pageId -=1;
+    for (let i=(quotes.length -1); i >= 0; i--){
+        sTarget.removeChild(quotes[i]);}; 
+    req.open("GET", `https://favqs.com/api/quotes?page=${pageId}&filter=${searchText}`);
+    req.setRequestHeader("Authorization", `Token token="${apiKey}"`);
+    req.send();}
+});
+
+req.open("GET", `https://favqs.com/api/quotes?page=${pageId}&filter=${searchText}`);
+req.setRequestHeader("Authorization", `Token token="${apiKey}"`);
+req.send();
+})
+
+
