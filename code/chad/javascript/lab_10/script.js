9261f5dbe9e1fc35739051ce3946c223505da1c9
Chad Bean <dabeanman808@gmail.com>
2018-12-12 13:28:36 -0800

    lab_10

---
+++ code/chad/javascript/lab_10/script.js
@@ -0,0 +1,41 @@
+let dTarget = document.getElementById("dtarget"); //Define Where output text is going to
+let displayBtn = document.getElementById("display-btn"); //assumes you have a button in your html
+let apiUser = ""
+let apiLocation = "portland"
+let apiUrl = `https://api.github.com/search/users?q=${apiUser}${apiLocation}`;
+let userInputLocation = document.getElementById("user-input-location");
+
+function displayFields() {
+
+}
+
+function runAPI() {
+    displayBtn.addEventListener("click", function(e) {
+        let req = new XMLHttpRequest();
+        req.addEventListener("progress", function(e) {
+            console.log(e.loaded);
+            dTarget.innerText = "Loading...";
+        });
+        req.addEventListener("error", function(e) {
+            console.log(e.status);
+            dTarget.innerText = "Cannot load quote. Try again later!";
+        });
+        req.addEventListener("load", function(e) {
+            dTarget.innerText = ""
+            console.log(req.responseText);
+            let response = JSON.parse(req.responseText);
+            console.log(response);
+            let resultHTML = `
+                <p>${response.quote.body}</p>
+                <p><i><a href="${response.quote.url}">${response.quote.author}</a></i></p>
+                `
+            textTarget.innerHTML = resultHTML;
+        });
+        req.open("GET", `${apiUrl}`); //url is defined on top
+        req.setRequestHeader("Authorization", `${apikey}`); // apikey is hidden in config.js
+        req.send()
+});
+}
+
+displayFields()
+
