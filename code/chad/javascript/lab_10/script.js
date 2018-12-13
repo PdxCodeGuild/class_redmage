b8ac7436913079ddbd9523e2c5d8c1424c843c4a
Chad Bean <crb4u@yahoo.com>
2018-12-13 12:17:20 -0800

    lab10

--- code/chad/javascript/lab_10/script.js
+++ code/chad/javascript/lab_10/script.js
@@ -1,29 +1,30 @@
-let dTarget = document.getElementById("dtarget"); //Define Where output text is going to
+//Define Where output text is going to
+let dTarget = document.getElementById("dtarget"); 
 
+//where the results of the output is to be displayed
 let displayBtn = document.getElementById("input-display-btn"); 
 
+//div sections that are hidden by defualt until javascript displays them
 let divUserName = document.getElementById("div-user-name"); 
 let divUserLocation = document.getElementById("div-user-location"); 
 let divUserPlanguauge = document.getElementById("div-user-planguage"); 
 
+//user input fields
 let inputUserInputName = document.getElementById("user-input-name"); 
 let inputUserInputLocation = document.getElementById("user-input-location"); 
 let inputUserInputPlanguauge = document.getElementById("user-input-planguauge"); 
 
-
+//checkboxes to be clicked which have event listeners to display div
 let chkBoxName = document.getElementById("chk-box-name")
 let chkBoxLocation = document.getElementById("chk-box-location")
 let chkBoxPlanguage = document.getElementById("chk-box-programming-languauge")
 
+//setting up variable to use used later
 var chkBoxNameInputValue;
 var chkBoxLocationInputValue;
 var chkBoxPlanguaugeInputValue;
 
-
-
-
-
-
+//event listeners to display divs onces checkboxes are checked
 function displayFields() {
     chkBoxName.addEventListener("change", function() { 
         if(this.checked) {
@@ -53,6 +54,8 @@ function displayFields() {
         
 }
 
+
+//runs API call after user inputs some information
 function runAPI() {
     let apiUrl = `https://api.github.com/search/users?q=${chkBoxNameInputValue}+location:${chkBoxLocationInputValue}`;
     console.log(apiUrl)
@@ -82,8 +85,9 @@ function runAPI() {
         req.send();
 }
 
+
+//once display button is clicked, if checkbox is checked, grab input data before calling api
 function searchGit() {
-    
     displayBtn.addEventListener("click", function() {
         if (chkBoxName.checked == true) {
             chkBoxNameInputValue = inputUserInputName.value;
