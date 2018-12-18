a6e48926258abb294fa39cb13f970011fb1c23ab
jeffrey <jeff.r.leslie@gmail.com>
2018-12-18 12:56:45 -0800

    d

--- code/jeffrey/polls-tutorial/polls/views.py
+++ code/jeffrey/polls-tutorial/polls/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
+
 def index(request):
-    return HttpResponse("Hello World")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = '<h1>' + '</h1><h1> '.join([q.question_text for q in latest_question_list]) + '</h1>'
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+def results(requst, question_id):
+    return HttpResponse(f"You're looking at the results of question {question_id}.")
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
+
