711b4b35d80623ab8f819f5622bb6b3166900332
Chad Bean <crb4u@yahoo.com>
2019-01-01 09:43:08 -0800

    adding

--- code/chad/django/grocery_list/grocery_list/gl_app/models.py
+++ code/chad/django/grocery_list/grocery_list/gl_app/models.py
@@ -6,10 +6,11 @@ from django.utils import timezone
 # text description, a created date, a completed date, and a boolean representing whether it was completed.
 
 class GroceryItem(models.Model):
-    name = models.CharField(max_length=32) #name of grocery item
+    gcname = models.CharField(max_length=32) #name of grocery item
     create_date = models.DateTimeField(default=timezone.now) #created date
     complete_date = models.DateTimeField(null=True, blank=True) #completed date
     done = models.BooleanField(default=False) #is completed?
 
     def __str__(self):  #returns the string representation of self name
-        return self.name
+        return self.gcname
+
--- code/chad/django/grocery_list/grocery_list/gl_app/views.py
+++ code/chad/django/grocery_list/grocery_list/gl_app/views.py
@@ -1,11 +1,18 @@
 
 from django.http import HttpResponse #For loading pure html without using a template
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
-from gl_app.models import GroceryItem
+from .models import GroceryItem
+from django.urls import reverse
+
 
 def index(request):
     template = loader.get_template('index.html')
     context = {
         'items': GroceryItem.objects.all()
     }
+    grocery_text = request.POST['gcname']
+    GroceryItem.objects.create(gcname=grocery_text)
     return HttpResponse(template.render(context, request))
+
+
